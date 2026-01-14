1e44c77acc2b8f089214c5c9d4d95c2c22fc50b0
adnanalameer16 <adnanalameer8@gmail.com>
2026-01-14 12:18:57 +0530

    PaymentRequest DTO for payment processing

---
+++ src/main/java/com/orders/order_management_service/dto/PaymentRequest.java
@@ -0,0 +1,15 @@
+package com.orders.order_management_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+
+    private String orderId;
+    private String paymentId;
+}
